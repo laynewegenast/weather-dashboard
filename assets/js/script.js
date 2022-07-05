cda4b7262580ecf220cce74429ceb97bce59a081
macbeth <layne.wegenast@gmail.com>
2022-07-04 21:40:45 -0500

    started challenge 6

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// open weather api key
+var apiID = "3915bb75e2967e4d45ad85de1463bcf9"
+
+var userInput = document.querySelector("#user-input");
+var selectedCity = document.querySelector("#selected-city");
+var cityDisplay = document.querySelector("#city-display");
+var currentWeather = document.querySelector("#current-weather");
+var uvIndex = document.querySelector("#uv-index");
+var fiveDayForecast = document.querySelector("#5-day-forecast");
+
